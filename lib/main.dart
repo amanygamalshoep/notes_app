5dbabad51290c16514e7ea9bb5bdb762fdf94764
Abdallah Ali <abdallahalifci91295@gmail.com>
2024-10-02 23:22:33 +0300

    add simple bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/Cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/constants.dart';
 import 'package:notes_app/model/note_model.dart';
+import 'package:notes_app/simple_bloc_observer.dart';
 import 'package:notes_app/views/notes_view.dart';
 
 void main() async {
+  Bloc.observer = SimpleBlocObserver();
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
   Hive.registerAdapter(NoteModelAdapter());
@@ -20,9 +22,9 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-     providers: [
-      BlocProvider(create: (context)=>AddNoteCubit()),
-     ],
+      providers: [
+        BlocProvider(create: (context) => AddNoteCubit()),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
