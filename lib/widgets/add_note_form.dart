64281438cc8843a461a16957b24b3193d1f1149e
Abdallah Ali <abdallahalifci91295@gmail.com>
2024-10-07 04:38:05 +0300

    formate date time

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:notes_app/Cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/model/note_model.dart';
 import 'package:notes_app/widgets/add_note_bottom_sheet.dart';
@@ -20,7 +21,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
   final GlobalKey<FormState> formKey = GlobalKey();
   AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
   String? title, subTitle;
- 
+
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -49,11 +50,12 @@ class _AddNoteFormState extends State<AddNoteForm> {
               onTap: () {
                 if (formKey.currentState!.validate()) {
                   formKey.currentState!.save();
+                  var currentDate = DateTime.now();
+                  var formatted = DateFormat.yMd().format(currentDate);
                   var note = NoteModel(
                       title: title!,
                       subTitle: subTitle!,
-                      date: DateTime.now().toString(),
-                      
+                      date:formatted ,
                       color: Colors.blue.value);
 
                   BlocProvider.of<AddNoteCubit>(context).addNote(note);
